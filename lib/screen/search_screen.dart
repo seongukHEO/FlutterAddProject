9c09f73cfb8cc4577473fb4bf3e9ff7c59c93263
seongukHEO <heoseonguk3@gmail.com>
2024-07-05 04:32:49 +0900

    feat : 상단 검색 부분 구현

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -6,8 +6,71 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Icon(Icons.search, size: 100,),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchTextBar(),
+            SearchGrid()
+          ],
+        ),
+      ),
     );
   }
 }
+
+class SearchTextBar extends StatelessWidget {
+  const SearchTextBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 60,
+        child: TextField(
+          cursorColor: Colors.black,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            hintText: "검색",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.white, width: 1)
+            ),
+
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(color: Colors.grey.shade200, width: 1)
+            ),
+            contentPadding: EdgeInsets.all(5),
+            filled: true,
+            fillColor: Colors.grey.shade200,
+
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final gridItems = List.generate(30, (index){
+  Colors.green.shade300;
+});
+
+
+class SearchGrid extends StatelessWidget {
+  const SearchGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: EdgeInsets.zero,
+        children: [
+          gridItems.map((color) => Container(color: color)).toList(),
+        ],
+        crossAxisCount: crossAxisCount
+    );
+  }
+}
+
+
